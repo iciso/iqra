d88ce71cc7f853052323800138c3cb4873e6cd2d
Rafique <69503725+iciso@users.noreply.github.com>
2025-07-11 08:15:13 +0530

    Update page.tsx

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";  
 import { useSearchParams } from "next/navigation";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
