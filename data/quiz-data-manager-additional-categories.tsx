40ed0d3eafe8f801c69f161f455b0ba0c517e168
Rafique <69503725+iciso@users.noreply.github.com>
2025-07-14 11:08:10 +0530

    Update quiz-data-manager-additional-categories.tsx

--- data/quiz-data-manager-additional-categories.tsx
+++ data/quiz-data-manager-additional-categories.tsx
@@ -1,5 +1,6 @@
 import type { QuizCategory } from "@/types/quiz"
 
-// Additional quiz categories are Tazkiyah 
+// Additional quiz categories here were removed but can be used for beefing up first 6 cats
+// First six cats are Quran Fiqh Tafsir Hadeeth Aqeedah Seerah having only 10 easy and 10 Advanced
 export const additionalCategories: QuizCategory[] = [
   ]
