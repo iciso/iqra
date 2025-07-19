d62aab4baf1862dbc1f54555abdc66d8a50fd9f2
Rafique <69503725+iciso@users.noreply.github.com>
2025-07-19 21:27:38 +0530

    Update page.tsx
    scale reduced for Meta logo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -182,7 +182,7 @@ export default function AboutPage() {
             <li>
               <strong>Community & Contributors:</strong> Immense gratitude to all open-source contributors, Quran scholars, Arabic linguists, and community members who have provided feedback, questions, and support to make IQRA a global platform for Islamic learning. Shout out to our prospective members who would join our yet to start Groups and Channels in WhatsApp, and Community pages in Fb and other Meta platforms. May Allah <span className="text-3xl font-normal align-middle">ﷻ</span> accept their efforts.
                  <div className="mt-2">
-                  <img src="/meta.svg" alt="Meta Logo" className="h-8 w-auto inline-block hover:scale-105 transition-transform dark:invert" />
+                  <img src="/meta.svg" alt="Meta Logo" className="h-8 w-auto inline-block hover:scale-95 transition-transform dark:invert" />
                   </div>
             </li>
           </ul>
