081ede2d6bd901d292f823d224a59f8d8faaae6f
Rafique <icisoi.club@gmail.com>
2025-08-21 20:53:03 +0530

    Create i18n-provider.tsx to wrap components with I18nextProvider:
    Create i18n-provider.tsx to wrap components with I18nextProvider:

---
+++ app/i18n-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'ta'],
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  });
+
+export default function I18nProvider({ children }: { children: React.ReactNode }) {
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+}
