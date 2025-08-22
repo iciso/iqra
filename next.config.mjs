3935a97f6b0d05f178a4b1ab9d00ae0014db947f
Rafique <icisoi.club@gmail.com>
2025-08-22 09:23:10 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  i18n: {
+    locales: ['en', 'ta'],
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
 };
 
-export default nextConfig; 
+export default nextConfig;
