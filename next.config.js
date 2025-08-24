06c392b32c7c824f34abd2f06bf5c0a784d2ff5b
Rafique <icisoi.club@gmail.com>
2025-08-24 20:10:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
   },
 };
-
-export default nextConfig; 
