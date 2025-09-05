6d9d727da786a4a1fc4f8811f731c8c3c2d004e4
Rafique <icisoi.club@gmail.com>
2025-09-05 22:34:42 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,35 +1,17 @@
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const code = searchParams.get('code');
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get("code")
 
   if (code) {
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          get(name) {
-            return cookies().get(name)?.value;
-          },
-          set(name, value, options) {
-            cookies().set(name, value, options);
-          },
-          remove(name, options) {
-            cookies().set(name, '', { ...options, maxAge: 0 });
-          },
-        },
-      }
-    );
-
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (error) {
-      return NextResponse.redirect('/auth?error=Authentication_failed');
-    }
+    const cookieStore = cookies()
+    const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect('/profile');
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(`${requestUrl.origin}/`)
 }
