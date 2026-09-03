29943f68d6b4797627e8b2d8a633909a3f091950
Rafique <icisoi.club@gmail.com>
2026-09-03 09:36:46 +0530

    Add dynamic export to challenge results page

--- app/challenge-results/[challengeId]/page.tsx
+++ app/challenge-results/[challengeId]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from "react"
 import { useParams, useRouter } from "next/navigation"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
